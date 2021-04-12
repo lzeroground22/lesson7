14b231c722577f7748a3c42dec1e086ac5385ef9
Alex <User@Big-PC.les.local>
2021-04-12 15:36:23 +0300

    work version

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from pprint import pprint
-
 cookbook = {
-    # Название блюда: [кол-во ингридиентов в блюде{
+    # Название блюда: [{
     #    'ingredient_name': 'имя',
     #    'quantity': число,
     #    'measure': 'единицы_измерения'
@@ -9,25 +7,16 @@ cookbook = {
     # пустая строка
 }
 
-
-def blanker():
-    pass
-
-
 with open('recipes.txt') as f:
-    dish_name = f.readline().strip()
-    count = int(f.readline())
-    ing_list = list()
-    for some in range(count):
-        ingr = f.readline().strip()
-        splited = ingr.split('|')
-        # print(splited)
-        ing_dict = {'ingredient_name': splited[0], 'quantity': splited[1], 'measure': splited[2]}
-        ing_list.append(ing_dict)
-    f.readline()
-    cookbook[dish_name] = ing_list
-
-# print(type(dish_name))
-# print(type(cookbook))
-# print(ing_dict)
+    for lin in f:
+        dish_name = lin.strip()
+        count = int(f.readline())
+        ing_list = list()
+        for some in range(count):
+            ingr = f.readline().strip()
+            splited = ingr.split('|')
+            ing_dict = {'ingredient_name': splited[0], 'quantity': splited[1], 'measure': splited[2]}
+            ing_list.append(ing_dict)
+        f.readline()
+cookbook[dish_name] = ing_list
 print(cookbook)
